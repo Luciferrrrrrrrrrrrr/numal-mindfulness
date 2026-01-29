52cc93075a6f4394cfa291ad51d7c8253ebbff23
Biswajeet Kumar <thisisbiswajeetkumar@gmail.com>
2026-01-29 17:41:47 +0530

    add eleven labs

---
+++ components/screens/SessionScreen.tsx
@@ -0,0 +1,55 @@
+import { Button, Text, View } from "react-native";
+import {useConversation} from "@elevenlabs/react-native"
+import { useUser } from "@clerk/clerk-expo";
+
+export default function SessionScreen() {
+    const {user} = useUser();
+    const conversation = useConversation({
+  onConnect: () => console.log('Connected to conversation'),
+  onDisconnect: () => console.log('Disconnected from conversation'),
+  onMessage: (message) => console.log('Received message:', message),
+  onError: (error) => console.error('Conversation error:', error),
+  onModeChange: (mode) => console.log('Conversation mode changed:', mode),
+  onStatusChange: (prop) => console.log('Conversation status changed:', prop.status),
+  onCanSendFeedbackChange: (prop) =>
+    console.log('Can send feedback changed:', prop.canSendFeedback),
+  onUnhandledClientToolCall: (params) => console.log('Unhandled client tool call:', params),
+});
+
+    const startConversation = async() => {
+        try { 
+            await conversation.startSession({
+                agentId: (process.env.EXPO_PUBLIC_AGENT_ID),
+                dynamicVariables:{
+                    user_name: user?.username ?? "Biswajeet",
+                    session_title: "test",
+                    session_description: "test",
+                },
+
+            });
+        } catch (e) {
+
+            console.log(e);
+        }
+    };
+
+    const endConversation = async() => {
+        try {
+            await conversation.endSession();
+        }
+            catch(e) {
+                console.log(e);
+            }
+        };
+    
+        
+    return (
+        <View>
+            <Text>
+                Session Screen
+            </Text>
+            <Button title="Start Conversation " onPress={startConversation}/>
+            <Button title="End Conversation " onPress={endConversation} color={"red"}/>
+        </View>
+    );
+}
